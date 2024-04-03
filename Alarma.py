8cab80d528b77cfc192e3da598eb09e1dd5d59c8
Pablo Pasin <pablopasinmassa@gmail.com>
2024-04-03 07:47:29 -0300

    Primer commit

---
+++ Alarma.py
@@ -0,0 +1,15 @@
+import os
+import time
+from playsound import playsound
+
+def alarma(minutes, seconds):
+    sound_path = os.path.join(os.path.dirname(__file__), "alarma_python.mp3")
+    total_seconds = minutes * 60 + seconds
+    print(f"La alarma sonará en {minutes} minutos y {seconds} segundos.")
+    time.sleep(total_seconds)
+    playsound(sound_path)
+    
+minutes = int(input("Ingresa los minutos de la alarma: "))
+seconds = int(input("Ingresa los segundos de la alarma: "))
+
+alarma(minutes, seconds)
